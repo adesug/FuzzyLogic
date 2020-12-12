480f96680244cda6ac7766325f87a28b705d03f4
adesug <adesugiantoro89@gmail.com>
2020-12-13 02:16:37 +0700

    First

---
+++ js/myscript.js
@@ -0,0 +1,17 @@
+(function($){
+
+     // jQuery for page scrolling feature - requires jQuery Easing plugin
+     $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+})(jQuery);
