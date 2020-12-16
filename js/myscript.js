a53209cb7209edd5223c339d2946d3cfbb7dbc82
adesug <adesugiantoro89@gmail.com>
2020-12-17 01:57:05 +0700

    fix layout

--- js/myscript.js
+++ js/myscript.js
@@ -1,17 +1,15 @@
-(function($){
+//event pada saat link di klik
 
-     // jQuery for page scrolling feature - requires jQuery Easing plugin
-     $('a.page-scroll').bind('click', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 50)
-        }, 1250, 'easeInOutExpo');
-        event.preventDefault();
-    });
+$('.page-scroll').on('click', function(event){
+    //ambil href
+    var tujuan = $(this).attr('href');
+    //tangkap element
+    var elementTujuan = $(tujuan);
+    //pindah scroll
+    $('html,body').animate({
+        scrollTop: elementTujuan.offset().top - 50
+    }, 1500, 'easeInOutExpo');
 
-    // Highlight the top nav as scrolling occurs
-    $('body').scrollspy({
-        target: '.navbar-fixed-top',
-        offset: 51
-    });
-})(jQuery);
+    event.preventDefault();
+
+});
